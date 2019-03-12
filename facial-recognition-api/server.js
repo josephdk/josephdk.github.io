8fe47fa52a2d4329df04a9f7b4257f9c2df60132
Joseph <joseph@josephdk.com>
2019-03-12 17:41:52 -0400

    Connecting front to back-end initalization

--- facial-recognition-api/server.js
+++ facial-recognition-api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -40,6 +43,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -80,6 +86,8 @@ app.put('/image', (req, res) => {
     }
 })
 
+
+
 app.listen(3000, () => {
-    console.log('hi');
+    console.log('Server is operational.');
 })
