9a008341d41aae0bc3645f9f98f67fb67f6f6b8b
Joseph <joseph@josephdk.com>
2019-03-12 00:11:02 -0400

    Finished each api request

--- facial-recognition-api/server.js
+++ facial-recognition-api/server.js
@@ -51,6 +51,35 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(users => {
+        if (users.id === id) {
+            found = true;
+            return res.json(users);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(users => {
+        if (users.id === id) {
+            found = true;
+            users.entries++
+            return res.json(users.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
 app.listen(3000, () => {
     console.log('hi');
 })
